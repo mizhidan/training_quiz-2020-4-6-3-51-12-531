60c34e292d14573e049f747075b97d878323cad8
mizhidan <dj9824@126.com>
2020-04-08 14:50:33 +0800

    [danjie] feat: DbUtil & excptions

--- src/main/java/exception/InvalidTicketException.java
+++ src/main/java/exception/InvalidTicketException.java
@@ -1,5 +1,15 @@
 package exception;
 
 public class InvalidTicketException extends RuntimeException {
+    public InvalidTicketException() {
 
+    }
+
+    public InvalidTicketException(String message) {
+        super(message);
+    }
+
+    public InvalidTicketException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
--- src/main/java/exception/ParkingLotFullException.java
+++ src/main/java/exception/ParkingLotFullException.java
@@ -2,4 +2,14 @@ package exception;
 
 public class ParkingLotFullException extends RuntimeException {
 
+    public ParkingLotFullException() {
+    }
+
+    public ParkingLotFullException(String message) {
+        super(message);
+    }
+
+    public ParkingLotFullException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
